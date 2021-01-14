d15924e147f1754ab6db2510f9b69ea44ec9c79d
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2021-01-14 18:25:44 +0100

    Profile follow function added. Error pages added.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,4 +13,5 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
